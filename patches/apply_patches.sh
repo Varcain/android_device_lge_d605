5a99ca8d9de6d926d2f98297ba97715e986bcc04
Kamil Lulko <rev13@wp.pl>
2014-02-22 18:48:16 +0100

    Fix voice call microphone volume

--- patches/apply_patches.sh
+++ patches/apply_patches.sh
@@ -3,6 +3,7 @@
 cp backlight_earlysuspend.patch ../../../../frameworks/base
 cp bluetooth_dbus_wait.patch ../../../../frameworks/base
 cp ril.patch ../../../../hardware/ril
+cp voicecall_mic.patch ../../../../hardware/qcom/audio
 
 cd ../../../../frameworks/base
 patch -p1 <backlight_earlysuspend.patch
@@ -10,3 +11,7 @@ patch -p1 <bluetooth_dbus_wait.patch
 
 cd ../../hardware/ril
 patch -p1 <ril.patch
+
+cd ../qcom/audio
+patch -p1 <voicecall_mic.patch
+
